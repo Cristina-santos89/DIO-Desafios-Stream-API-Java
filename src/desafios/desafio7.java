5c182314c49fc3e94266a5f7b4adb4e9c94fd15f
Cristina-santos89 <cris.manfredine@hormail.com>
2023-10-06 00:36:46 -0300

    Desafios Stream API Java

--- src/desafios/desafio7.java
+++ src/desafios/desafio7.java
@@ -1,24 +1,25 @@
 package desafios;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.stream.Collectors;
 
 public class desafio7 {
 public static void main(String[] args) throws Exception {
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-
-
     
     //Desafio 7 - Encontrar o segundo número maior da lista:
 
-     numeros.stream()
-    .limit(2)
-    .collect(Collectors.toSet());
-
-
-
+Integer segundoNumero = numeros.stream()
+    .distinct()  // Remove elementos repetidos
+    .sorted(Comparator.reverseOrder())  // Ordena os números na ordem decrescente
+    .skip(1)  // Pula o primeiro número
+    .findFirst()  // Encontra o primeiro elemento após o pulo
+    .orElseThrow(() -> new NoSuchElementException("Não existe segundo maior número."));
+    System.out.println(segundoNumero);
     
 }
 }
