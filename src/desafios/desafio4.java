c176236107439e0e518cb10fd32f87bc1241b87a
Cristina-santos89 <cris.manfredine@hormail.com>
2023-10-05 22:54:59 -0300

    Desafios Stream API Java

--- src/desafios/desafio4.java
+++ src/desafios/desafio4.java
@@ -3,6 +3,7 @@ package desafios;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 
 public class desafio4 {
@@ -13,16 +14,12 @@ public class desafio4 {
 
 // Desafio 4 - Remova todos os valores ímpares:
 
-    //System.out.println("Removendo os números ímpares da lista: ");
-    //numeros1.removeIf(i -> i % 2 != 0);
-    //System.out.println(numeros1);
+    System.out.println("Removendo os números ímpares da lista: ");
+        List<Integer> numerosParesComZero = numeros.stream()
+        .filter(n -> n % 2 == 0 || n == 0) // Filtra números pares e o zero
+        .collect(Collectors.toList());
 
-
-    System.out.println("Remova os valores ímpares: ");
-    numeros1 = numeros.removeIf(integer -> integer % 2 != 0);
-    System.out.println(numeros);
-    //numeros1.removeIf(i -> (i % 2 != 0));
-   // System.out.println(numeros1);
+        System.out.println("Números pares e neutro na lista: " + numerosParesComZero);
 
     
 }
